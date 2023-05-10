9dc4293901f3eb2ee60fba8fea652d7432a3cf01
miltonpl <miltonpalaguachi@gmail.com>
2023-05-10 18:07:34 -0400

    Added DSpace

---
+++ Platform/Designify/Project/Designify/Sources/DSpace.swift
@@ -0,0 +1,25 @@
+//
+//  DesignifySpace.swift
+//  Designify
+//
+//  Created by Milton Palaguachi on 5/9/23.
+//
+
+import Foundation
+/// Designify Space
+public enum DSpace {
+    public static let space2: CGFloat = 2.0
+    public static let space4: CGFloat = 4.0
+    public static let space6: CGFloat = 6.0
+    public static let space8: CGFloat = 8.0
+    public static let space10: CGFloat = 10.0
+    public static let space16: CGFloat = 16.0
+    public static let space20: CGFloat = 20.0
+    public static let space24: CGFloat = 24.0
+    public static let space28: CGFloat = 28.0
+    public static let space32: CGFloat = 32.0
+    public static let space36: CGFloat = 36.0
+    public static let space40: CGFloat = 40.0
+    public static let space44: CGFloat = 44.0
+    public static let space48: CGFloat = 48.0
+}
