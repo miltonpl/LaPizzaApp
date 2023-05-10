953da61eea4ca47542db5e93c6355d0f4488e64c
miltonpl <miltonpalaguachi@gmail.com>
2023-05-10 18:03:20 -0400

    Added Icons to Designify Framework bundle

---
+++ Platform/Designify/Project/Designify/Designify.h
@@ -0,0 +1,18 @@
+//
+//  Designify.h
+//  Designify
+//
+//  Created by Milton Palaguachi on 5/9/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Designify.
+FOUNDATION_EXPORT double DesignifyVersionNumber;
+
+//! Project version string for Designify.
+FOUNDATION_EXPORT const unsigned char DesignifyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Designify/PublicHeader.h>
+
+
