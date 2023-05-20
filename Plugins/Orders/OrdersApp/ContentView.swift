fc14c165c46be5e406f5000462f438f215ebfc77
miltonpl <miltonpalaguachi@gmail.com>
2023-05-19 23:28:19 -0400

    add project to orders framework

--- Plugins/Orders/OrdersApp/ContentView.swift
+++ Plugins/Orders/OrdersApp/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  OrdersApp
 //
-//  Created by Milton Palaguachi on 5/11/23.
+//  Created by Milton Palaguachi on 5/19/23.
 //
 
 import SwiftUI
--- Plugins/Orders/OrdersApp/OrdersApp.swift
+++ Plugins/Orders/OrdersApp/OrdersApp.swift
@@ -1,14 +1,14 @@
 //
-//  OrdersAppApp.swift
+//  OrdersApp.swift
 //  OrdersApp
 //
-//  Created by Milton Palaguachi on 5/11/23.
+//  Created by Milton Palaguachi on 5/19/23.
 //
 
 import SwiftUI
 
 @main
-struct OrdersAppApp: App {
+struct OrdersApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
